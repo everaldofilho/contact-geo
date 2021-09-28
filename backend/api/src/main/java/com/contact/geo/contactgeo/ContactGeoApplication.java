fe49d10a7e7f4a2c42f6fca022d894d7411790c0
claudio-code <suporte2424@outlook.com>
2021-09-28 15:51:09 -0300

    adicionada a api e o worker

---
+++ backend/api/src/main/java/com/contact/geo/contactgeo/ContactGeoApplication.java
@@ -0,0 +1,13 @@
+package com.contact.geo.contactgeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactGeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactGeoApplication.class, args);
+    }
+
+}
---
+++ backend/api/src/test/java/com/contact/geo/contactgeo/ContactGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.geo.contactgeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactGeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
