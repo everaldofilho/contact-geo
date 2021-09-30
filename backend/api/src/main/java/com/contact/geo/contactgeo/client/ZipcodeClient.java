77db11f4c09293a1ea4df6e37075d44303451c93
claudio-code <suporte2424@outlook.com>
2021-09-30 09:16:39 -0300

    ajustado para pegar a url do application.properties

--- backend/api/src/main/java/com/contact/geo/contactgeo/client/ZipcodeClient.java
+++ backend/api/src/main/java/com/contact/geo/contactgeo/client/ZipcodeClient.java
@@ -2,26 +2,27 @@ package com.contact.geo.contactgeo.client;
 
 import com.contact.geo.contactgeo.dto.searchzipcode.ZipcodeResponseDTO;
 import com.contact.geo.contactgeo.exception.ZipcodeException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class ZipcodeClient {
-    private static final String URI = "https://viacep.com.br/ws/";
 
-    private WebClient webClient;
+    @Value("${app.cep-client.uri}")
+    private String uri;
+
+    private final WebClient webClient;
 
     public ZipcodeClient(WebClient.Builder builder) {
-        this.webClient = builder
-                .baseUrl(URI)
-                .build();
+        this.webClient = builder.build();
     }
 
     public ZipcodeResponseDTO getZipcode(String zipcode) {
         return webClient
                 .method(HttpMethod.GET)
-                .uri(URI + zipcode + "/json/")
+                .uri(uri, zipcode)
                 .retrieve()
                 .bodyToMono(ZipcodeResponseDTO.class)
                 .doOnError(this::handleError)
